0c1da1265d3f149835d6372086e67e23855501ed
Connor O'Brien <connor@hifranc.com>
2020-12-29 19:02:00 +0200

    Add script file

---
+++ script.js
@@ -0,0 +1,9 @@
+var enteredName = "harry";
+var requiredName = "Harry";
+
+if (enteredName.toUpperCase() === requiredName.toUpperCase()) {
+    console.log("Name accepted");
+}
+else {
+    console.log("Name not accepted");
+}
